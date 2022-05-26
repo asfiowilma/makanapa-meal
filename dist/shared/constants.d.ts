de2d4a5c001528b59fbab2ba2a886b6b58d112c4
asfiowilma <asfiolithaw@yahoo.co.id>
2022-05-26 20:51:11 +0700

    🚀 deployment build

--- dist/shared/constants.d.ts
+++ dist/shared/constants.d.ts
@@ -1,2 +1,2 @@
 export declare const INVALID_USER = -1;
-export declare const AUTH_SERVICE_URL = "https://makanapa-be.herokuapp.com";
+export declare const AUTH_SERVICE_URL = "http://34.123.46.127:8080";
--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,2 +1,2 @@
 export const INVALID_USER = -1
-export const AUTH_SERVICE_URL = 'https://makanapa-be.herokuapp.com'
+export const AUTH_SERVICE_URL = 'http://34.123.46.127:8080'
